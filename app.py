953445094a960d84f7166d513a6277a35d8f974e
渥美太瑠斗 <atsumifutoshi@atsumifutoshinoMacBook-puro.local>
2019-03-17 18:08:02 +0900

    default

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=message))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
