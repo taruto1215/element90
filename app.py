664cb74d4cb42b723d39a261054aa7441caf7a26
taruto atsumi <taruto.atsumi@gmail.com>
2019-03-21 12:32:22 +0900

    default

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def callback():
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #Responses when a message came=================
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=ImageMessage)
 def handle_message(event):
 
     message = element.get_ele(event.message.text.rstrip())
@@ -83,23 +83,22 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=message))
 
-@handler.add(MessageEvent, message=ImageMessage)
-def imagesendmessage():
-    image_message = ImageSendMessage(
-        original_content_url='http://res.cloudinary.com/demo/image/upload/w_250,h_250,c_fill,f_auto/seagull.jpg',
-        preview_image_url='http://res.cloudinary.com/demo/image/upload/w_250,h_250,c_fill,f_auto/seagull.jpg'
-    )
-    return (image_message)
-
-if (event.message.text == '周期表') or (event.message.text == 'periodic table'):
-    image_message = imagesendmessage()
-    line_bot_api.reply_message(
-        event.reply_token,
+    def imagesendmessage():
         image_message = ImageSendMessage(
             original_content_url='http://res.cloudinary.com/demo/image/upload/w_250,h_250,c_fill,f_auto/seagull.jpg',
             preview_image_url='http://res.cloudinary.com/demo/image/upload/w_250,h_250,c_fill,f_auto/seagull.jpg'
         )
-    )
+        return (image_message)
+
+    if (event.message.text == '周期表') or (event.message.text == 'periodic table'):
+        image_message = imagesendmessage()
+        line_bot_api.reply_message(
+            event.reply_token,
+            image_message = ImageSendMessage(
+                original_content_url='http://res.cloudinary.com/demo/image/upload/w_250,h_250,c_fill,f_auto/seagull.jpg',
+                preview_image_url='http://res.cloudinary.com/demo/image/upload/w_250,h_250,c_fill,f_auto/seagull.jpg'
+            )
+        )
 
 #preview_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Electron_shell_001_Hydrogen_%28diatomic_nonmetal%29_-_no_label.svg/200px-Electron_shell_001_Hydrogen_%28diatomic_nonmetal%29_-_no_label.svg.png'
 #=============================================
